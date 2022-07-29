d8291455088de28d60fa7de0fd3211c24573f177
Marius <marius@example.com>
2022-07-29 14:42:34 +0300

    init

---
+++ js/class/Todo.js
@@ -0,0 +1,15 @@
+export default class Todo {
+  // savybe pridedama prie klases on ne prie klases objektu
+  static todoCount = 0;
+
+  constructor(newTitle) {
+    this.id = `t_${++Todo.todoCount}`;
+    this.title = newTitle;
+    this.complete = false;
+    this.timeStamp = new Date().toLocaleTimeString();
+  }
+
+  markDone() {
+    this.complete = true;
+  }
+}
