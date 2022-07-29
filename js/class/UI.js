ad51ed6d4d75d5759d57051f8687fbe61aa30e58
Marius <marius@example.com>
2022-07-29 14:42:26 +0300

    oop start

---
+++ js/class/UI.js
@@ -0,0 +1,7 @@
+class UI {
+  static inputEl = document.getElementById('new-todo');
+  static addTodoBtnEl = document.getElementById('add-todo');
+  static mainListEl = document.getElementById('list-todo');
+
+  makeNewTodo() {}
+}
